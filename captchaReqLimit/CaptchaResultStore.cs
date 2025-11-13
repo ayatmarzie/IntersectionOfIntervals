56e4995875b8e22fa6a1d8f03954a1ca796ad12d
ayatmarzie <ayat.marzie@gmail.com>
2025-11-13 18:00:46 +0330

    Initial commit for Captcha

---
+++ captchaReqLimit/CaptchaResultStore.cs
@@ -0,0 +1,23 @@
+﻿public class CaptchaResultStore
+{
+    private readonly List<string> _responses = new();
+    private readonly object _lock = new();
+
+    public void Add(string message)
+    {
+        lock (_lock)
+        {
+            _responses.Add(message);
+            if (_responses.Count > 50)
+                _responses.RemoveAt(0); // keep last 50
+        }
+    }
+
+    public List<string> GetAll()
+    {
+        lock (_lock)
+        {
+            return _responses.ToList();
+        }
+    }
+}
